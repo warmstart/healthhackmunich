6313ff4b10a6178b74236e9da7d808e824bccfda
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-15 22:41:53 +0100

    Views for userprofile

--- challenge/fitbitchallenge/urls.py
+++ challenge/fitbitchallenge/urls.py
@@ -3,15 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import userprofile.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fitbitchallenge.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^(?P<user_id>\d+)/$', userprofile.views.viewProfile, name='viewProfile'),
 )
--- challenge/userprofile/views.py
+++ challenge/userprofile/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
+from userprofile.models import user
 
 # Create your views here.
 def viewProfile(request, user_id):
-    u = get_object_or_404(pk=user_id)
-    return render(request, 'userprofile/layout.html', u)
+    u = get_object_or_404(user, pk=user_id)
+    return render(request, 'userprofile/userprofile.html', u)
