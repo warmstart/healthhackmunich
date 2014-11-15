c1ccc6290f4934dcd7368369f77c3499421036b8
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-15 17:57:39 +0100

    Started implementing data model

--- challenge/challenge/models.py
+++ challenge/challenge/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Challenge(models.Model):
+    challenge_name = models.CharField(max_length=200)
+    creator = models.CharField(max_length=200)
+    stepsgoal = models.IntegerField(default=0)
+    deadline = models.DateTimeField()
+
+class ChallengeType(models.Model):
+    
+
+class ChallengeParticipation(models.Model):
+    user = models.ForeignKey('users.user')
+    challenge = models.ForeignKey(Challenge)
+    steps = models.IntegerField(default=0)
+    status = models.CharField(max_length=200)
+
--- challenge/userprofile/models.py
+++ challenge/userprofile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+def Users(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+    gender = models.CharField(max_length=1)
+
+
