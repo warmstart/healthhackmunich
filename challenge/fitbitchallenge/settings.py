7cd81e09ba6961bcdf5b013d3fe2c5b0f88e34db
Maciek Kazirod <kazirod.maciej@gmail.com>
2014-11-15 21:28:40 +0100

    local run on sqlite

--- challenge/fitbitchallenge/settings.py
+++ challenge/fitbitchallenge/settings.py
@@ -87,7 +87,12 @@ STATIC_URL = '/static/'
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+try:
+    os.environ["LOCAL"]
+except:
+    DATABASES['default'] =  dj_database_url.config()
+
+print(DATABASES)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
