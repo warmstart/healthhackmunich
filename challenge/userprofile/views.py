ecc8114c08d9dd632f37fcc7b050229f1eb42855
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-15 22:36:16 +0100

    userprofile view

--- challenge/userprofile/views.py
+++ challenge/userprofile/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+
 
 # Create your views here.
+def viewProfile(request, user_id):
+    u = get_object_or_404(pk=user_id)
+    return render(request, 'userprofile/layout.html', u)
