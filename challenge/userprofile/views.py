5a0eb54f3ac0d00c01388bd8540e7fa639b4678f
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-15 22:46:39 +0100

    fixup on views

--- challenge/userprofile/views.py
+++ challenge/userprofile/views.py
@@ -7,4 +7,5 @@ from userprofile.models import user
 # Create your views here.
 def viewProfile(request, user_id):
     u = get_object_or_404(user, pk=user_id)
-    return render(request, 'userprofile/userprofile.html', u)
+    context = {'username' : u.name}
+    return render(request, 'userprofile/userprofile.html', context)
