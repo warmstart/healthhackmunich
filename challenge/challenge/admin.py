0b81362b737a723163b76053a45fcfde5611ac54
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-15 20:20:46 +0100

    fixup modesl

--- challenge/challenge/admin.py
+++ challenge/challenge/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from challenge.models import Challenge, ChallengeParticipation
+from userprofile.models import user
 
 # Register your models here.
+admin.site.register(Challenge)
+admin.site.register(ChallengeParticipation)
+admin.site.register(user)
--- challenge/userprofile/models.py
+++ challenge/userprofile/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
+class user(models.Model):
     name = models.CharField(max_length=50)
     picture = models.CharField(max_length=200)
     location = models.CharField(max_length=100)
     age = models.IntegerField(default=0)
     gender = models.CharField(max_length=1)
 
-
+    def __unicode__( self ):
+        return "{0}".format(self.name)
