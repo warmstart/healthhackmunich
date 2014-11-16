72e23178601713dd71f5be69196cbfd913d72478
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-11-16 07:34:15 +0100

    fixup

--- challenge/badges/models.py
+++ challenge/badges/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class badges(models.Model):
-    picture = models.CharacterField(max_length=200)
-    name    = models.CharacterField(max_length=200)
+    picture = models.CharField(max_length=200)
+    name    = models.CharField(max_length=200)
 
 
 class badgesEarnt(models.Model):
